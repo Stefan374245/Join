f0096a7c44180caf6df8809fe7a388ab8613b8a0
Stefan Helldobler <shelldobler@gmx.de>
2025-12-15 18:01:56 +0100

    feat: add reusable ClickOutsideDirective for global use

---
+++ src/app/shared/directives/click-outside.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, EventEmitter, HostListener, Output } from '@angular/core';
+
+/**
+ * Directive to detect clicks outside of an element
+ * Usage: <div (clickOutside)="onClickOutside()">...</div>
+ */
+@Directive({
+  selector: '[clickOutside]',
+  standalone: true
+})
+export class ClickOutsideDirective {
+  @Output() clickOutside = new EventEmitter<void>();
+
+  constructor(private elementRef: ElementRef) {}
+
+  @HostListener('document:click', ['$event.target'])
+  public onClick(target: HTMLElement): void {
+    const clickedInside = this.elementRef.nativeElement.contains(target);
+    if (!clickedInside) {
+      this.clickOutside.emit();
+    }
+  }
+}
