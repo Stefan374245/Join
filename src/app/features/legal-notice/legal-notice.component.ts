c6515b9f298ecd97c411922a4256a00aaab447fa
Stefan Helldobler <shelldobler@gmx.de>
2026-02-02 13:41:24 +0100

    refactor: create shared info-pages styles for help/legal/privacy

--- src/app/features/legal-notice/legal-notice.component.ts
+++ src/app/features/legal-notice/legal-notice.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink} from '@angular/router';
+
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
--- src/app/features/privacy-policy/privacy-policy.component.ts
+++ src/app/features/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
 
+  scrollTo(elementId: string): void {
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
