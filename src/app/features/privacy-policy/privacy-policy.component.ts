2393c96f4568d1facf83cf124d496bc1c151c37c
Stefan Helldobler <shelldobler@gmx.de>
2026-02-04 18:38:24 +0100

    fix(privacy-policy): remove unused RouterLink import

--- src/app/features/privacy-policy/privacy-policy.component.ts
+++ src/app/features/privacy-policy/privacy-policy.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
